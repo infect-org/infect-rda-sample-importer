ce98fe7001136658f0c8f7e30a35048bb858d3e0
Michael van der Weg <michael@joinbox.com>
2018-07-20 17:00:07 +0200

    chore: addd integration config

---
+++ config/config.integration.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+const envr = require('envr');
+
+
+module.exports = {
+    port: 8010,
+    registryHost: 'http://l.dns.porn:9000',
+    apiHost: 'http://api.l.dns.porn:8000',
+};
